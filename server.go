b70e6de4baaf0296d66f26efdbf4857e82ede0de
Swasti Mishra <swasti.mishra0101@gmail.com>
2024-07-12 15:18:55 +0530

    modifed server.go

--- server.go
+++ server.go
@@ -15,7 +15,6 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-// Message defines the structure of our messages
 type Message struct {
 	Username  string `json:"username"`
 	Message   string `json:"message"`
@@ -23,17 +22,10 @@ type Message struct {
 }
 
 func main() {
-	// Configure WebSocket route
 	http.HandleFunc("/ws", handleConnections)
-
-	// Start listening for incoming chat messages
 	go handleMessages()
-
-	// Serve static files (optional, if you have an HTML file to serve)
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/", fs)
-
-	// Start the server on localhost port 3000 and log any errors
 	log.Println("http server started on :3000")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
@@ -42,35 +34,28 @@ func main() {
 }
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
-	// Upgrade initial GET request to a WebSocket
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer ws.Close()
-
-	// Register new client
 	clients[ws] = true
 
 	for {
 		var msg Message
-		// Read new message as JSON and map it to a Message object
 		err := ws.ReadJSON(&msg)
 		if err != nil {
 			log.Printf("error: %v", err)
 			delete(clients, ws)
 			break
 		}
-		// Send new message to the broadcast channel
 		broadcast <- msg
 	}
 }
 
 func handleMessages() {
 	for {
-		// Grab the next message from the broadcast channel
 		msg := <-broadcast
-		// Send it out to every client that is currently connected
 		for client := range clients {
 			err := client.WriteJSON(msg)
 			if err != nil {
