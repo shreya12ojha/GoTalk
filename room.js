cc6c5e42b09984566a2a73466727370f04617849
Swasti Mishra <swasti.mishra0101@gmail.com>
2024-07-12 12:29:23 +0530

    added js code for asking room number and name

---
+++ room.js
@@ -0,0 +1,9 @@
+function joinRoom(room) {
+    const username = document.getElementById('username-input').value;
+    if (username) {
+      window.location.href = `chat.html?username=${username}&room=${room}`;
+    } else {
+      alert("Please enter your name");
+    }
+  }
+  
