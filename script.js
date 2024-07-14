6b08a54a70c39b18d88e99d2f17a8b5940d44982
Shreya <ojhashreya2006@gmail.com>
2024-07-14 14:04:20 +0000

    updated the javascript file to set cookies

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const urlParams = new URLSearchParams(window.location.search);
 const username = urlParams.get('username');
 const room = urlParams.get('room');
 
+getCookie("username");
+if (username) {
+    console.log("Welcome back, " + username);
+} else {
+    console.log("Hello, new user");
+}
+
 // Ensure username and room are present
 if (!username || !room) {
   alert("Username and room are required");
@@ -65,3 +72,27 @@ function displayMessage(messages) {
 window.onload = function() {
   document.getElementById("messages").scrollTop = document.getElementById("messages").scrollHeight;
 };
+
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+setCookie("username", "exampleUser", 1);
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
